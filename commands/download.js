9578067d99abeed7f4dc25f770b78152d08d9375
ewanhowell5195 <48070995+ewanhowell5195@users.noreply.github.com>
2022-08-10 23:55:08 +0100

    Update download.js

--- commands/download.js
+++ commands/download.js
@@ -8,8 +8,7 @@ export default {
     try {
       const site = cheerio.load(await fetch("https://optifine.net/downloads").then(e => e.text()))
       const version = site(".downloadLine:first")
-      let name = version.find(".colFile").text()
-      name = name.replace("OptiFine", `OptiFine ${site("h2:first").text().split(" ")[1]}`)
+      const name = version.find(".colFile").text().replace("OptiFine", `OptiFine ${site("h2:first").text().split(" ")[1]}`)
       const url = "https://optifine.net/" + cheerio.load(await fetch(version.find(".colMirror").children().attr("href")).then(e => e.text()))(".downloadButton").children().attr("href")
       fields.push(["Latest release", `[${name}](${url})`])
     } catch {}
